f5512d44cd90d7fe15071822872c4cac2afc3cf4
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-08-28 16:07:01 +0800

    updated

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,78 @@
+var btnConnect = document.getElementById('btn_connect');
+var btnPublish = document.getElementById('btn_publish');
+var inputTopicPub = document.querySelector('.input-topic-pub');
+var inputTopic = document.getElementById('topic').value;
+var inputPayload = document.getElementById('payload').value;
+var Status = document.getElementById('stat');
+
+
+
+
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+btnConnect.addEventListener('click', function (e) {
+  e.preventDefault();
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  client.subscribe("mqtt/demo")
 
-client.on("connect", function(){
+  client.on("connect", function () {
     console.log("Successfully connected");
-})
+  })
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
+  btnPublish.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log("message")
+  client.on("message", function (topic, payload) {
+    console.log([topic, payload].join(": "));
+    // client.end();
+  })
 
-client.publish("mqtt/demo", "hello world!")
+    client.publish(inputTopic, inputPayload);
+  });
+
+  });
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+// import paho.mqtt.client as mqtt
+
+
+// def on_connect(client, userdata, flags, rc):
+//   print("Connected with result code "+str(rc))
+
+
+// #def on_message(client, userdata, msg):
+// #   print(msg.topic+" "+str(msg.payload))
+
+// client = mqtt.Client()
+// client.on_connect = on_connect
+// #client.on_message = on_message
+
+// client.connect("iot.eclipse.org", 1883, 60)
+// #client.connect("broker.hivemq.com", 1883, 60)
+// while True:
+//   message = input("").split(":")
+//   if(len(message)==1):	#for all
+//     client.publish("  cay",message[0])
+//   if (len(message)==2): #for specefic receiver
+//     message[0]="cay"
+//     client.publish("r/"+message[0], message[1])
+
+
+// client.loop_forever()
+
+
+
 
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
